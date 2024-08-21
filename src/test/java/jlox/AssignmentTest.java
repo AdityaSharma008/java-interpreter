81ca138fa8c0193b9903bce5254e51cb74891076
Aditya Sharma <121496528+AdityaSharma008@users.noreply.github.com>
2024-08-21 16:36:57 +0530

    Add tests for assignment operator

---
+++ src/test/java/jlox/AssignmentTest.java
@@ -0,0 +1,93 @@
+package jlox;
+
+import org.junit.jupiter.api.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AssignmentTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+    private static final String lineSepartor = System.lineSeparator();
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+    @Test
+    @DisplayName("Test right associativity")
+    void testAssociativity(){
+        String script = """
+                var a = "a";
+                var b = "b";
+                var c = "c";
+                                
+                a = b = c;
+                print a; // expect: c
+                print b; // expect: c
+                print c; // expect: c
+                """;
+
+        helper(script);
+
+        String expectedOutput = "c" + lineSepartor + "c" + lineSepartor + "c" + lineSepartor;
+
+        assertEquals(expectedOutput, outContent.toString());
+    }
+
+    @Test
+    @DisplayName("Throw error for undefined variable")
+    void testUndefinedVariable(){
+        String script = """
+                undef = 123;
+                """;
+
+        helper(script);
+
+        String expectedOutput = "[line 1]: Undefined Variable 'undef'." + lineSepartor;
+
+        assertEquals(expectedOutput, errContent.toString());
+    }
+
+    @Test
+    @DisplayName("Check scope")
+    void testScope(){
+        String script = """
+                var a = 10;
+                {
+                    print a;
+                    var a = 20;
+                    print a;
+                }
+                print a;
+                """;
+
+        helper(script);
+
+        String expectedOutput = "10" + lineSepartor + "20" + lineSepartor + "10" + lineSepartor;
+
+        assertEquals(expectedOutput, outContent.toString());
+    }
+
+    void helper(String script){
+        Scanner scanner = new Scanner(script);
+        List<Token> tokens = scanner.scanTokens();
+        Parser parser = new Parser(tokens);
+        List<Stmt> statements = parser.parse();
+        Interpreter interpreter = new Interpreter();
+        interpreter.interpret(statements);
+    }
+}
