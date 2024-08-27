62b3d6abe802b71a897414968e40a73896902439
Aditya Sharma <121496528+AdityaSharma008@users.noreply.github.com>
2024-08-27 23:05:02 +0530

    Add tests for conditional statements

---
+++ src/test/java/jlox/ConditionalsTest.java
@@ -0,0 +1,61 @@
+package jlox;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ConditionalsTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+    private static final String lineSeparator = System.lineSeparator();
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+    @DisplayName("Check dangling else")
+    @ParameterizedTest
+    @MethodSource("danglingElse")
+    void checkDanglingElse(String sourceUnderTest, String expected){
+        helper(sourceUnderTest);
+        expected += lineSeparator;
+        assertEquals(expected, outContent.toString());
+    }
+
+    void helper(String script){
+        Scanner scanner = new Scanner(script);
+        List<Token> tokens = scanner.scanTokens();
+        Parser parser = new Parser(tokens);
+        List<Stmt> statements = parser.parse();
+        Interpreter interpreter = new Interpreter();
+        interpreter.interpret(statements);
+    }
+
+    private static Stream<Arguments> danglingElse(){
+        return Stream.of(
+                Arguments.of("if (true) if (false) print \"bad\"; else print \"good\";", "good"),
+                Arguments.of("if (true) if (true) print \"bad\"; else print \"good\";", "bad")
+        );
+    }
+}
